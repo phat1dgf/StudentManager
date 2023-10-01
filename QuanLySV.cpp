8c6a3b5bcff1fc840f1edbbf368c875d56917b63
qiji2206 <phamphat2262@gmail.com>
2023-10-01 12:23:13 +0700

    Update QuanLySV.cpp

--- QuanLySV.cpp
+++ QuanLySV.cpp
@@ -69,22 +69,18 @@ public:
     // them
     void add(string hoten, int tuoi, double diem, string mssv)
     {
+        Node *newSinhVien = new Node();
+        newSinhVien->sinhvien.hoTen = hoten;
+        newSinhVien->sinhvien.Diem = setDiem(diem);
+        newSinhVien->sinhvien.Tuoi = setTuoi(tuoi);
+        newSinhVien->sinhvien.MSSV = mssv;
         if (isEmpty())
         {
-            Node *newSinhVien = new Node();
-            newSinhVien->sinhvien.hoTen = hoten;
-            newSinhVien->sinhvien.Diem = setDiem(diem);
-            newSinhVien->sinhvien.Tuoi = setTuoi(tuoi);
-            newSinhVien->sinhvien.MSSV = mssv;
+
             head = newSinhVien;
         }
         else
         {
-            Node *newSinhVien = new Node();
-            newSinhVien->sinhvien.hoTen = hoten;
-            newSinhVien->sinhvien.Diem = setDiem(diem);
-            newSinhVien->sinhvien.Tuoi = setTuoi(tuoi);
-            newSinhVien->sinhvien.MSSV = mssv;
             Node *cur = head;
             while (cur->next != NULL)
             {
@@ -100,68 +96,67 @@ public:
         if (isEmpty())
         {
             cout << "Hien tai danh sach sinh vien khong co du lieu" << endl;
+            return;
         }
-        else
+
+        Node *cur = head;
+        while (cur != NULL)
         {
-            Node *cur = head;
-            while (cur != NULL)
+            if (mssv == cur->sinhvien.MSSV)
             {
-                if (mssv == cur->sinhvien.MSSV)
+                int option;
+                do
                 {
-                    int option;
-                    do
+                    cout << "1 - Ten" << endl;
+                    cout << "2 - Tuoi" << endl;
+                    cout << "3 - Diem" << endl;
+                    cout << "4 - MSSV" << endl;
+                    cout << "0 - Thoat" << endl;
+                    cout << "Nhap lua chon: ";
+                    cin >> option;
+                    switch (option)
                     {
-                        cout << "1 - Ten" << endl;
-                        cout << "2 - Tuoi" << endl;
-                        cout << "3 - Diem" << endl;
-                        cout << "4 - MSSV" << endl;
-                        cout << "0 - Thoat" << endl;
-                        cout << "Nhap lua chon: ";
-                        cin >> option;
-                        switch (option)
-                        {
-                        case 1:
-                        {
-                            string ten;
-                            cout << "Nhap ten: ";
-                            cin >> ten;
-                            cur->sinhvien.hoTen = ten;
-                            break;
-                        }
-                        case 2:
-                        {
-                            int tuoi;
-                            cout << "Nhap tuoi: ";
-                            cin >> tuoi;
-                            cur->sinhvien.Tuoi = tuoi;
-                            break;
-                        }
-                        case 3:
-                        {
-                            double diem;
-                            cout << "Nhap diem: ";
-                            cin >> diem;
-                            cur->sinhvien.Diem = diem;
-                            break;
-                        }
-                        case 4:
-                        {
-                            string MSSV;
-                            cout << "Nhap MSSV: ";
-                            cin >> MSSV;
-                            cur->sinhvien.MSSV = MSSV;
-                            break;
-                        }
-                        default:
-                            break;
-                        }
-                    } while (option != 0);
-                    break;
-                }
-                else
-                {
-                    cur = cur->next;
-                }
+                    case 1:
+                    {
+                        string ten;
+                        cout << "Nhap ten: ";
+                        cin >> ten;
+                        cur->sinhvien.hoTen = ten;
+                        break;
+                    }
+                    case 2:
+                    {
+                        int tuoi;
+                        cout << "Nhap tuoi: ";
+                        cin >> tuoi;
+                        cur->sinhvien.Tuoi = tuoi;
+                        break;
+                    }
+                    case 3:
+                    {
+                        double diem;
+                        cout << "Nhap diem: ";
+                        cin >> diem;
+                        cur->sinhvien.Diem = diem;
+                        break;
+                    }
+                    case 4:
+                    {
+                        string MSSV;
+                        cout << "Nhap MSSV: ";
+                        cin >> MSSV;
+                        cur->sinhvien.MSSV = MSSV;
+                        break;
+                    }
+                    default:
+                        break;
+                    }
+                } while (option != 0);
+                break;
+            }
+            else
+            {
+                cur = cur->next;
             }
         }
     }
@@ -172,42 +167,41 @@ public:
         if (isEmpty())
         {
             cout << "Hien tai danh sach sinh vien khong co du lieu" << endl;
+            return;
         }
-        else
+
+        Node *cur = head;
+        while (cur != NULL)
         {
-            Node *cur = head;
-            while (cur != NULL)
+            if (mssv == cur->sinhvien.MSSV && head == cur)
             {
-                if (mssv == cur->sinhvien.MSSV && head == cur)
-                {
-                    head = cur->next;
-                    cur->next = NULL;
-                    head->pre = NULL;
-                    delete cur;
-                    cout << "1" << endl;
-                    break;
-                }
-                else if (mssv == cur->sinhvien.MSSV && cur != head && cur->next != NULL)
-                {
-                    Node *preNode = cur->pre;
-                    Node *nextNode = cur->next;
-                    preNode->next = nextNode;
-                    nextNode->pre = preNode;
-                    delete cur;
-                    cout << "2" << endl;
-                    break;
-                }
-                else if (mssv == cur->sinhvien.MSSV && cur->next == NULL)
-                {
-                    Node *preNode = cur->pre;
-                    preNode->next = NULL;
-                    delete cur;
-                    cout << "3" << endl;
-                }
-                else
-                {
-                    cur = cur->next;
-                }
+                head = cur->next;
+                cur->next = NULL;
+                head->pre = NULL;
+                delete cur;
+                cout << "1" << endl;
+                break;
+            }
+            else if (mssv == cur->sinhvien.MSSV && cur != head && cur->next != NULL)
+            {
+                Node *preNode = cur->pre;
+                Node *nextNode = cur->next;
+                preNode->next = nextNode;
+                nextNode->pre = preNode;
+                delete cur;
+                cout << "2" << endl;
+                break;
+            }
+            else if (mssv == cur->sinhvien.MSSV && cur->next == NULL)
+            {
+                Node *preNode = cur->pre;
+                preNode->next = NULL;
+                delete cur;
+                cout << "3" << endl;
+            }
+            else
+            {
+                cur = cur->next;
             }
         }
     }
@@ -217,34 +211,33 @@ public:
         if (isEmpty())
         {
             cout << "Hien tai danh sach sinh vien khong co du lieu" << endl;
+            return;
         }
-        else
+
+        Node *cur = head;
+        int stt = 1;
+        cout << setw(5) << "STT"
+             << setw(15) << "MSSV"
+             << setw(20) << "Ten"
+             << setw(10) << "Tuoi"
+             << setw(10) << "Diem" << endl;
+
+        // In dòng ngăn cách
+        cout << setfill('-') << setw(60) << "-" << endl;
+        cout << setfill(' '); // Đặt lại fill character về khoảng trắng
+        while (cur->next != NULL)
         {
-            Node *cur = head;
-            int stt = 1;
-            cout << setw(5) << "STT"
-                 << setw(15) << "MSSV"
-                 << setw(20) << "Ten"
-                 << setw(10) << "Tuoi"
-                 << setw(10) << "Diem" << endl;
-
-            // In dòng ngăn cách
-            cout << setfill('-') << setw(60) << "-" << endl;
-            cout << setfill(' '); // Đặt lại fill character về khoảng trắng
-            while (cur->next != NULL)
+            if (cur->sinhvien.hoTen.find(textToFind) >= 0 && cur->sinhvien.hoTen.find(textToFind) <= cur->sinhvien.hoTen.length())
             {
-                if (cur->sinhvien.hoTen.find(textToFind) >= 0 && cur->sinhvien.hoTen.find(textToFind) <= cur->sinhvien.hoTen.length())
-                {
 
-                    cout << setw(5) << stt
-                         << setw(15) << cur->sinhvien.MSSV
-                         << setw(20) << cur->sinhvien.hoTen
-                         << setw(10) << cur->sinhvien.Tuoi
-                         << setw(10) << cur->sinhvien.Diem << endl;
-                    stt++;
-                }
-                cur = cur->next;
+                cout << setw(5) << stt
+                     << setw(15) << cur->sinhvien.MSSV
+                     << setw(20) << cur->sinhvien.hoTen
+                     << setw(10) << cur->sinhvien.Tuoi
+                     << setw(10) << cur->sinhvien.Diem << endl;
+                stt++;
             }
+            cur = cur->next;
         }
     }
     // in
@@ -254,32 +247,31 @@ public:
         if (isEmpty())
         {
             cout << "Hien tai danh sach sinh vien khong co du lieu" << endl;
+            return;
         }
-        else
+
+        int stt = 1;
+        Node *cur = head;
+        // In tiêu đề của bảng
+        cout << setw(5) << "STT"
+             << setw(15) << "MSSV"
+             << setw(20) << "Ten"
+             << setw(10) << "Tuoi"
+             << setw(10) << "Diem" << endl;
+
+        // In dòng ngăn cách
+        cout << setfill('-') << setw(60) << "-" << endl;
+        cout << setfill(' '); // Đặt lại fill character về khoảng trắng
+        while (cur != NULL)
         {
-            int stt = 1;
-            Node *cur = head;
-            // In tiêu đề của bảng
-            cout << setw(5) << "STT"
-                 << setw(15) << "MSSV"
-                 << setw(20) << "Ten"
-                 << setw(10) << "Tuoi"
-                 << setw(10) << "Diem" << endl;
 
-            // In dòng ngăn cách
-            cout << setfill('-') << setw(60) << "-" << endl;
-            cout << setfill(' '); // Đặt lại fill character về khoảng trắng
-            while (cur != NULL)
-            {
-
-                cout << setw(5) << stt
-                     << setw(15) << cur->sinhvien.MSSV
-                     << setw(20) << cur->sinhvien.hoTen
-                     << setw(10) << cur->sinhvien.Tuoi
-                     << setw(10) << cur->sinhvien.Diem << endl;
-                stt++;
-                cur = cur->next;
-            }
+            cout << setw(5) << stt
+                 << setw(15) << cur->sinhvien.MSSV
+                 << setw(20) << cur->sinhvien.hoTen
+                 << setw(10) << cur->sinhvien.Tuoi
+                 << setw(10) << cur->sinhvien.Diem << endl;
+            stt++;
+            cur = cur->next;
         }
     }
     // sap xep
@@ -343,55 +335,55 @@ public:
         cout << "Diem trung binh toan bo sinh vien: " << diemTong / tongSoSinhVien << endl;
         cout << "Tuoi trung binh: " << (double)tongTuoi / tongSoSinhVien << endl;
     }
-    void save()
-    {
-        string filename = "QLSV.txt";
-        ofstream file(filename);
-        if (file.is_open())
-        {
-            Node *cur = head;
-            while (cur != NULL)
-            {
-                file << cur->sinhvien.hoTen << ",";
-                file << cur->sinhvien.MSSV << ",";
-                file << cur->sinhvien.Tuoi << ",";
-                file << cur->sinhvien.Diem << endl;
+    //     void save()
+    //     {
+    //         string filename = "QLSV.txt";
+    //         ofstream file(filename);
+    //         if (file.is_open())
+    //         {
+    //             Node *cur = head;
+    //             while (cur != NULL)
+    //             {
+    //                 file << cur->sinhvien.hoTen << ",";
+    //                 file << cur->sinhvien.MSSV << ",";
+    //                 file << cur->sinhvien.Tuoi << ",";
+    //                 file << cur->sinhvien.Diem << endl;
 
-                cur = cur->next;
-            }
-            file.close();
-            cout << "Danh sach sinh vien da duoc luu" << filename << endl;
-        }
-        else
-            cout << "Khong the luu danh sach sinh vien" << endl;
-    }
-    void load()
-    {
-        string filename = "QLSV.txt";
-        ifstream file(filename);
-        // Check if the file is open
-        if (!file.is_open())
-        {
-            cerr << "Failed to open " << filename << std::endl;
-            return; // Exit with an error code
-        }
+    //                 cur = cur->next;
+    //             }
+    //             file.close();
+    //             cout << "Danh sach sinh vien da duoc luu" << filename << endl;
+    //         }
+    //         else
+    //             cout << "Khong the luu danh sach sinh vien" << endl;
+    //     }
+    //     void load()
+    //     {
+    //         string filename = "QLSV.txt";
+    //         ifstream file(filename);
+    //         // Check if the file is open
+    //         if (!file.is_open())
+    //         {
+    //             cerr << "Failed to open " << filename << std::endl;
+    //             return; // Exit with an error code
+    //         }
 
-        // Read and print the contents line by line
-        char delimiter = ',';
-        string line;
-        while (getline(file, line))
-        {
-            istringstream ss(line);
-            string x;
-            while (getline(ss, x, delimiter))
-            {
-                cout << x << endl;
-            }
-        }
+    //         // Read and print the contents line by line
+    //         char delimiter = ',';
+    //         string line;
+    //         while (getline(file, line))
+    //         {
+    //             istringstream ss(line);
+    //             string x;
+    //             while (getline(ss, x, delimiter))
+    //             {
+    //                 cout << x << endl;
+    //             }
+    //         }
 
-        // Close the file
-        file.close();
-    }
+    //         // Close the file
+    //         file.close();
+    //     }
 };
 
 int main()
@@ -475,7 +467,7 @@ int main()
         break;
         case 8:
         {
-            QuanLySV.save();
+            // QuanLySV.save();
         }
         break;
         case 9:
